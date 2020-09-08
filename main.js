85715324c612c9b9dcae180b590a2a92a96d1c18
Pzhicong <260056164@qq.com>
2020-09-08 11:22:09 +0800

    修改样式

--- main.js
+++ main.js
@@ -1,12 +1,38 @@
-import {myCreateEle,Component,render} from "./toy-react.js"
+import { myCreateEle, Component, render } from './toy-react.js'
 
-class MyComponent extends Component{
-    render(){
-        return <div><h1>my component </h1>{this.children}</div>
-    }
+class MyComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>my component </h1>
+        {this.children}
+      </div>
+    )
+  }
 }
 
-render(<MyComponent id="name" class="person">
+render(
+  <MyComponent id="name" class="person">
     <div name="wang">abc</div>
     <div name="li"></div>
-    </MyComponent>,document.body);
+  </MyComponent>,
+  document.body
+)
+
+myCreateEle(
+  'div',
+  {
+    id: 'name',
+    class: 'person',
+  },
+  myCreateEle(
+    'div',
+    {
+      name: 'wang',
+    },
+    'abc'
+  ),
+  myCreateEle('div', {
+    name: 'li',
+  })
+)
